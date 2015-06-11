1f9f5e8980b29e5c540badfb862c5b4c088c6903
Martin Kautz <kautz@jakota.de>
2015-06-11 13:16:55 +0200

    Added AckViewController

---
+++ Rang/AckViewController.swift
@@ -0,0 +1,57 @@
+//
+//  AckViewController.swift
+//  Rangliste-A-Swift
+//
+//  Created by Martin Kautz on 11.06.15.
+//  Copyright (c) 2015 JAKOTA Design Group. All rights reserved.
+//
+
+import UIKit
+
+class AckViewController: UIViewController {
+
+    // ---------------------------------------------------------------------------------------------
+    @IBOutlet weak var theTextView: UITextView!
+    // ---------------------------------------------------------------------------------------------
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+        title = "Danksagungen"
+        var  fontSize: CGFloat = 9.0
+
+        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+            fontSize = 15.0
+        }
+        theTextView.font = UIFont(name: "HelveticaNeue-Light", size: fontSize)
+
+        var sPathToAboutText = NSBundle.mainBundle().pathForResource("3rdparty", ofType: "txt")
+        var readError: NSError?
+
+        var theAboutText = NSString(contentsOfFile: sPathToAboutText!, encoding: NSUTF8StringEncoding, error: &readError)
+
+        self.theTextView.text = String(theAboutText!)
+        self.edgesForExtendedLayout = .None;
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
