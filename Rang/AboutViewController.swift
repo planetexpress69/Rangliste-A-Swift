33c993c2bd97a04fcb5fe5fc09dc37af6d3a45ce
Martin Kautz <kautz@jakota.de>
2015-06-11 11:11:34 +0200

    Added more resources

---
+++ Rang/AboutViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AboutViewController.swift
+//  Rangliste-A-Swift
+//
+//  Created by Martin Kautz on 11.06.15.
+//  Copyright (c) 2015 JAKOTA Design Group. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        title = "Über Rangliste"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
